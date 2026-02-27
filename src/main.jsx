eaecbee311f9438fd3a00b3bd61609e669f43386
gabrieltan0506-prog <gabrieltan0506@gmail.com>
2026-02-27 13:23:02 +0800

    init structure

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChartKLine from "./components/ChartKLine.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(<ChartKLine />);
